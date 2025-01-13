745127315980086d9fa63dfaaa85e456fd77bd82
ohhyeseong <ohs020105@gmail.com>
2025-01-13 20:59:29 +0900

    [fix] 오류 수정본

--- src/routes/partner/orders.router.js
+++ src/routes/partner/orders.router.js
@@ -1,19 +1,19 @@
 import express from 'express';
 import ordersController from '../../controllers/partner/orders.controller.js';
-import { authorization } from '../../middlewares/auth.middleware.js';
+import { authenticatePartner } from '../../middlewares/auth.middleware.js';
 
 const ordersRouter = express.Router();
 
 //매장 주문들 확인
-ordersRouter.get('/', authorization, ordersController.getOrders);
+ordersRouter.get('/', authenticatePartner, ordersController.getOrders);
 
 //주문한 메뉴 확인 (사장님)
-ordersRouter.get('/:orderId', authorization, ordersController.selectGetOrder);
+ordersRouter.get('/:orderId', authenticatePartner, ordersController.selectGetOrder);
 
 //주문 접수(수락) (사장님)
-ordersRouter.patch('/:orderId/status', authorization, ordersController.patchOrder);
+ordersRouter.patch('/:orderId/status', authenticatePartner, ordersController.patchOrder);
 
 //주문 취소 (사장님)
-ordersRouter.post('/:orderId/status', authorization, ordersController.cancelOrder);
+ordersRouter.post('/:orderId/status', authenticatePartner, ordersController.cancelOrder);
 
 export default ordersRouter;
