ed4eadd608ea358f3f9b1170d76775cfa15a4193
SmoothMonologue <mobugy99@gmail.com>
2025-01-14 13:05:56 +0900

    🩹fix: 인증 미들웨어 적용

--- src/routes/user/cart.router.js
+++ src/routes/user/cart.router.js
@@ -1,18 +1,19 @@
 import express from 'express';
 import cardController from '../../controllers/card.controller.js';
+import { authenticateUser } from '../../middlewares/auth.middleware.js';
 
 const cartsRouter = express.Router();
 
 //새로운 장바구니 추가
-cartsRouter.post('/', authorization, cardController.createCart);
+cartsRouter.post('/', authenticateUser, cardController.createCart);
 
 //장바구니에 선택한 메뉴 추가
-cartsRouter.post('/:cartId', authorization, cardController.newMenuOfCart);
+cartsRouter.post('/:cartId', authenticateUser, cardController.newMenuOfCart);
 
 //사용자의 장바구니 조회
-cartsRouter.get('/', authorization, cardController.usingCarts);
+cartsRouter.get('/', authenticateUser, cardController.usingCarts);
 
 //장바구니 삭제
-cartsRouter.delete('/:cartId', authorization, cardController.deleteCart);
+cartsRouter.delete('/:cartId', authenticateUser, cardController.deleteCart);
 
 export default cartsRouter;
