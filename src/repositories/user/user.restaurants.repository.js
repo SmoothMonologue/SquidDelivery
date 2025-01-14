1a8267f6c19d357ab66f1e66fbef8cc6789a84ff
강나연 <skdusdl4@naver.com>
2025-01-14 15:34:17 +0900

    [docs] 파일 수정

--- src/repositories/user/user.restaurants.repository.js
+++ src/repositories/user/user.restaurants.repository.js
@@ -1,4 +1,4 @@
-import { prisma } from '../utils/prisma/index.js';
+import { prisma } from '../../utils/prisma/index.js';
 import { RESTAURANT_MESSAGES } from '../../constants/message.constant.js';
 
 class UserRestaurantRepository {
