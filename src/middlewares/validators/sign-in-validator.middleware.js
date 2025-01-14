153e17503b79b6cfbdf2fe8837053ac76fef02ee
ohhyeseong <ohs020105@gmail.com>
2025-01-14 15:40:55 +0900

    [fix] 프리즈마 수정

--- src/middlewares/validators/sign-in-validator.middleware.js
+++ src/middlewares/validators/sign-in-validator.middleware.js
@@ -9,6 +9,9 @@ const schema = Joi.object({
   password: Joi.string().required().messages({
     'any.required': MESSAGES.AUTH.COMMON.PASSWORD.REQURIED,
   }),
+  catchBox: Joi.string().optional().messages({
+    'string.base': MESSAGES.AUTH.COMMON.CATCH_BOX.INVALID_FORMAT,
+    }),
 });
 
 export const signInValidator = async (req, res, next) => {
