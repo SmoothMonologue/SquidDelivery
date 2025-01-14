c995f6dfdfdc72de61dfa8f7de2ff214685bbaa0
lgm-7 <xlzlrud@naver.com>
2025-01-15 02:08:33 +0900

    fix

--- src/repositories/user/orders.repository.js
+++ src/repositories/user/orders.repository.js
@@ -8,7 +8,7 @@ class OrderRepository {
   }
 
   findCart = async (userId) => {
-    return await prisma.cart.findUnique({
+    return await prisma.cart.findFirst({
       where: { userId: +userId },
     });
   };
@@ -17,7 +17,10 @@ class OrderRepository {
     return await prisma.$transaction(async (tx) => {
       //결제api
 
-      return await tx.order.create({
+      const order = await tx.order.update({
+        where: {
+          cartId: cart.id,
+        },
         data: {
           userId: +userId,
           cartId: cart.id,
@@ -29,20 +32,20 @@ class OrderRepository {
     });
   };
 
-  cancelOrder = async (orderId, userId, tx) => {
-    if (tx) {
+  cancelOrderTransaction = async (orderId) => {
+    return await prisma.$transaction(async (tx) => {
       //결제취소 api
 
       return await tx.order.update({
-        where: { id: +orderId, userId: +userId },
+        where: { id: +orderId },
         data: { status: '주문 취소' },
       });
-    }
+    });
   };
 
-  checkOrderStatus = async (orderId, userId) => {
-    return await prisma.order.findUnique({
-      where: { id: +orderId, userId: +userId },
+  checkOrderStatus = async (orderId) => {
+    return await prisma.order.findFirst({
+      where: { id: +orderId },
       select: { status: true },
     });
   };
