f4761646c8cb59e620507e9dd8ff228596b699b1
강나연 <skdusdl4@naver.com>
2025-01-15 17:58:16 +0900

    [add] 파일 추가

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/index.js';
+import { Server } from 'socket.io';
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
 
 app.use('/api', routes);
 
