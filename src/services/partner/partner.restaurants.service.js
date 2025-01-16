cc53e8cc99e4690f1af5ea30469e7c4e3eee1441
didtjs123 <kjn90901@naver.com>
2025-01-16 19:25:01 +0900

    update 사냥님 메뉴 api기능 버그 수정

--- src/services/partner/partner.restaurants.service.js
+++ src/services/partner/partner.restaurants.service.js
@@ -7,11 +7,11 @@ export class PartnerRestaurantService {
     this.#repository = repository;
   }
 
-  async createRestaurant(data,id) {
+  async createRestaurant(data, id) {
     if (!data.restaurantName) {
       throw new Error(MESSAGES.RESTAURANTS.COMMON.REQUIRED_FIELDS);
     }
-    return this.#repository.createRestaurant(data,id);
+    return this.#repository.createRestaurant(data, id);
   }
 
   async updateRestaurant(id, data) {
@@ -46,4 +46,3 @@ export class PartnerRestaurantService {
     }
   }
 }
-
