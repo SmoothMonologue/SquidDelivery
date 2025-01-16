834d965a2003b269413ecc8cd206d05673fb8020
Hyeunggeun-Yun <gudrms121712@naver.com>
2025-01-16 21:11:31 +0900

    fix 로그인 실패시 생기는 에러 버그 해결

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,16 +7,22 @@ export class AuthController {
   constructor(service) {
     this.#service = service;
   }
-  userSignUp = async (req, res) => {
+  userSignUp = async (req, res, next) => {
     const { email, password, name, interest, phoneNumber } = req.body;
-    await this.#service.createUser({ email, password, name, interest, phoneNumber });
-    res.status(HTTP_STATUS.CREATED).json(MESSAGES.AUTH.SIGN_UP.SUCCEED);
+    const user = await this.#service.createUser({ email, password, name, interest, phoneNumber });
+    if (user.status) {
+      return res.status(user.status).json(user.message);
+    }
+    return res.status(HTTP_STATUS.CREATED).json(MESSAGES.AUTH.SIGN_UP.SUCCEED);
   };
 
   partnerSignUp = async (req, res) => {
     const { email, password, name, phoneNumber } = req.body;
-    await this.#service.createPartner({ email, password, name, phoneNumber });
-    res.status(HTTP_STATUS.CREATED).json(MESSAGES.AUTH.SIGN_UP.SUCCEED);
+    const partnet = await this.#service.createPartner({ email, password, name, phoneNumber });
+    if (partnet.status) {
+      return res.status(partnet.status).json(partnet.message);
+    }
+    return res.status(HTTP_STATUS.CREATED).json(MESSAGES.AUTH.SIGN_UP.SUCCEED);
   };
 
   signIn = async (req, res, next) => {
@@ -25,22 +31,20 @@ export class AuthController {
 
       if (catchBox) {
         const partner = await this.#service.signInPartner({ email, password });
-        // console.log(partner)
         const { accessToken } = partner.data;
         if (accessToken) {
           res.setHeader('Authorization', `Bearer ${accessToken}`);
           console.log('사장님 로그인 성공!');
         }
-        res.status(HTTP_STATUS.OK).json(partner.data);
+        return res.status(HTTP_STATUS.OK).json(partner.data);
       } else {
         const user = await this.#service.signInUser({ email, password });
-        // console.log(user);
         const { accessToken } = user.data;
         if (accessToken) {
           res.setHeader('authorization', `Bearer ${accessToken}`);
           console.log('사용자 로그인 성공!');
         }
-        res.status(HTTP_STATUS.CREATED).json(user.data);
+        return res.status(HTTP_STATUS.CREATED).json(user.data);
       }
     } catch (error) {
       next(error);
@@ -50,7 +54,8 @@ export class AuthController {
   signOut = async (req, res) => {
     const { authorization } = req.headers;
     const user = await this.#service.signOut({ authorization });
-    res.status(user.status).json(user.message);
+    if (user.status) {
+    }
+    return res.status(user.status).json(user.message);
   };
 }
-
